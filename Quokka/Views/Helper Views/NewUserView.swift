6806fc198e4f24334a59ecd5971a6617fc4aba5d
NicParsons <96993843+NicParsons@users.noreply.github.com>
2025-06-08 11:16:43 +1000

    Add NewUserView

---
+++ Quokka/Views/Helper Views/NewUserView.swift
@@ -0,0 +1,87 @@
+import SwiftUI
+import SwiftData
+
+struct NewUserView: View {
+	@Environment(\.modelContext) private var modelContext
+	// get all users for uniqueness check
+	@Query private var existingUsers: [User]
+	@State private var name = ""
+	@State private var errorMessage: String?
+	@Binding var done: Bool
+
+	var body: some View {
+		NavigationStack {
+			Form {
+				Section(header: Text("User Name")) {
+					TextField("Name", text: $name)
+					#if os(iOS)
+						.autocapitalization(.words)
+					#endif
+				} // section
+
+				if let errorMessage = errorMessage {
+					Section {
+						Text(errorMessage)
+							.foregroundColor(.red)
+					} // section
+				} // end if
+			} // form
+			.padding()
+			.navigationTitle("Create New User")
+			.toolbar {
+				ToolbarItem(placement: .confirmationAction) {
+					Button("Done") {
+						saveUser()
+					} // button
+					.disabled(!nameIsValid)
+				} // toolbar item
+
+				ToolbarItem(placement: .cancellationAction) {
+					Button("Cancel") {
+done = true
+					} // button
+				} // toolbar item
+			} // toolbar
+		} // nav stack
+	} // body
+
+	private func saveUser() {
+		guard nameIsValid else {
+return
+		}
+
+		let newUser = User(name: validatedName())
+		modelContext.insert(newUser)
+
+		do {
+			try modelContext.save()
+			done = true
+		} catch {
+			errorMessage = "Could not create new user: \(error.localizedDescription)"
+		} // do try catch
+	} // func
+
+	private func validatedName() -> String {
+		return name.trimmingCharacters(in: .whitespacesAndNewlines)
+	} // func
+} // view
+
+extension NewUserView {
+	var nameIsValid: Bool {
+		let trimmedName = validatedName()
+
+		guard !trimmedName.isEmpty else {
+			errorMessage = "A user name cannot be blank."
+			return false
+		}
+
+		let nameExists = existingUsers.contains { $0.name.caseInsensitiveCompare(trimmedName) == .orderedSame }
+
+		guard !nameExists else {
+			errorMessage = "A user with this name already exists."
+			return false
+		}
+
+		return true
+	} // var
+} // extension

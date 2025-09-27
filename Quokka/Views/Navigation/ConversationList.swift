fa35d21439cfdbec5b0c87f59dc29e4539d34143
NicParsons <96993843+NicParsons@users.noreply.github.com>
2025-09-27 17:25:46 +1000

    Fix selection in ConversationList side bar

--- Quokka/Views/Navigation/ConversationList.swift
+++ Quokka/Views/Navigation/ConversationList.swift
@@ -5,16 +5,17 @@ struct ConversationList: View {
 	@Environment(Model.self) private var model
 	@Environment(\.modelContext) private var context
 	@Environment(SessionManager.self) private var session
-	@State private var selectedUser: User? = nil
+	@State private var selectedUserID: User.ID? = nil
 	@State var selectedPost: Post? = nil
 	@Query private var users: [User]
 
 	var body: some View {
 		NavigationView {
 			HStack {
-				List(users, selection: $selectedUser) { user in
+				List(users, selection: $selectedUserID) { user in
 					Text(user.name.capitalized)
 				} // list
+				.listStyle(.sidebar)
 
 				CalendarList(author: selectedUser, selectedPost: $selectedPost)
 			} // HStack
@@ -23,3 +24,10 @@ struct ConversationList: View {
 		.navigationTitle("Conversation with \(selectedUser?.name ?? "?")")
 	} // body
 } // view
+
+
+extension ConversationList {
+	var selectedUser: User? {
+		users.first(where: { $0.id == selectedUserID } )
+	}
+} // extension
